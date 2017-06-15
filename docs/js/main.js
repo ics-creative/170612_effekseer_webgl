f1040586347fad0f8a47d449780b3159a1e7b5ee
Katushi Suzuki <suzuki@ics-web.jp>
2017-06-15 10:39:05 +0900

    SP時にFPSを落とす

---
+++ docs/js/main.js
@@ -0,0 +1,92 @@
+var isSp = getDevice()=="sp";
+var effects = {};
+var main = function () {
+var counter = 0;
+var _app = document.getElementById("app");
+var canvas = document.getElementById("canvas");
+var width  = canvas.width;
+var height = canvas.height;
+
+var scene = new THREE.Scene();
+
+var fov    = 30;
+var aspect = width / height;
+var near   = 1;
+var far    = 1000;
+var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+var target = new THREE.Vector3(0, 0, 0);
+camera.position.set( 0, 2, 40 );
+camera.lookAt(target);
+
+controls = new THREE.OrbitControls(camera);
+controls.autoRotate = false;
+
+var renderer = new THREE.WebGLRenderer({canvas: canvas, preserveDrawingBuffer: true});
+renderer.setSize( width, height );
+//document.body.appendChild( renderer.domElement );
+
+effekseer.init(renderer.context);
+effects["sword"] = effekseer.loadEffect("Resource/002_sword_effect/sword.efk");
+effects["Laser02"] = effekseer.loadEffect("Resource_/Laser01.efk");
+effects["TitleEffect"] = effekseer.loadEffect("Resource/004_logo_effect/TitleEffect.efk");
+effects["distortion"] = effekseer.loadEffect("Resource/005_distortion_effect/distortion.efk");
+effects["block"] = effekseer.loadEffect("Resource_/block.efk");
+
+var grid = new THREE.GridHelper(20, 10, 0xffffff, 0xffffff);
+//scene.add(grid);
+
+//var directionalLight = new THREE.DirectionalLight( 0xffffff );
+//directionalLight.position.set( 0, 0.7, 0.7 );
+//scene.add( directionalLight );
+
+( function renderLoop () {
+  // SPéûÇÕ30FPSÇ…Ç∑ÇÈ
+  if (isSp && ++counter%2==0){
+    if (!paused) {
+  	  effekseer.update();
+    }
+    requestAnimationFrame( renderLoop );
+  	return;
+  }
+  requestAnimationFrame( renderLoop );
+  
+  if (!paused) {
+  	effekseer.update();
+  }          
+  controls.update();
+  
+  renderer.render( scene, camera );
+  effekseer.setProjectionMatrix(camera.projectionMatrix.elements);
+  effekseer.setCameraMatrix(camera.matrixWorldInverse.elements);
+  effekseer.draw();
+
+} )();
+};
+window.addEventListener( 'DOMContentLoaded', main, false );
+var paused = false;
+function stopAll() {
+	paused = false;
+	effekseer.stopAll();
+}
+function togglePause () {
+	paused = !paused;
+	document.getElementById("buttonPause").innerHTML = paused? "çƒê∂":"É|Å[ÉY";
+}
+function toggleRotate () {
+	if (!controls) return;
+	controls.autoRotate = !controls.autoRotate;
+	document.getElementById("buttonRotate").innerHTML = controls.autoRotate? "é©ìÆâÒì]ON":"é©ìÆâÒì]OFF";
+}
+function capture () {
+	var canvas = window.document.getElementById("canvas");
+	var dataUrl = canvas.toDataURL();
+	window.open(dataUrl);
+}
+  
+var canvas = document.getElementById('canvas');
+var container = document.getElementById('app');
+sizing();
+function sizing() {
+	canvas.height = container.offsetHeight;
+	canvas.width = container.offsetWidth;
+}
